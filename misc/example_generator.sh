39f62671d30dc4863d6766e32762a9e8046ecb68
masukomi <masukomi@masukomi.org>
2022-12-30 10:19:35 -0500

    corrected echo output in example generator

--- misc/example_generator.sh
+++ misc/example_generator.sh
@@ -4,8 +4,8 @@ echo "github color scheme (default)"
 cat random_numbers.txt | heatmapper
 echo "darkhub color scheme"
 cat random_numbers.txt | heatmapper --scheme darkhub
-echo "darkhub color scheme"
+echo "bluehub color scheme"
 cat random_numbers.txt | heatmapper --scheme bluehub
-echo "darkhub color scheme"
+echo "wistia color scheme"
 cat random_numbers.txt | heatmapper --scheme wistia
 
